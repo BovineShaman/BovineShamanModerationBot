3dcc6f567428e2a5a4aa71a0b8038a23de36ae6d
BovineShaman <44685442+BovineShaman@users.noreply.github.com>
2021-01-01 00:16:05 -0500

    Initial Creation

---
+++ src/env.js
@@ -0,0 +1,4 @@
+require('dotenv').config()
+
+export const BOT_PREFIX = process.env.BOT_PREFIX
+export const DISCORD_TOKEN = process.env.DISCORD_TOKEN
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import {BOT_PREFIX, DISCORD_TOKEN} from "../env";
+
+let Discord = require("discord.js");
+
+export let botClient = new Discord.Client();
